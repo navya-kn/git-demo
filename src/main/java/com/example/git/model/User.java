bc6d80ebb4bc218db2eb0a8706356a7f79ce06c9
navya <navya.nataraj.k.n@gmail.com>
2025-02-15 16:08:39 +0530

    modified - added email field

--- src/main/java/com/example/git/model/User.java
+++ src/main/java/com/example/git/model/User.java
@@ -4,6 +4,7 @@ public class User {
 
     private int id;
     private String userName;
+    private  String email;
     private String password;
 
     public int getId() {
@@ -28,5 +29,13 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
